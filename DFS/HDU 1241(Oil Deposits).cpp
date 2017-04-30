252cbaac374a4e816eaa2164427e5dac5dbe2389
ZYSzys <1220736035@qq.com>
2017-04-30 22:32:17 +0800

    Update HDU 1241(Oil Deposits).cpp

--- DFS/HDU 1241(Oil Deposits).cpp
+++ DFS/HDU 1241(Oil Deposits).cpp
@@ -1,4 +1,4 @@
-Oil Deposits
+Oil Deposits(http://acm.hdu.edu.cn/showproblem.php?pid=1241)
 
 Time Limit: 2000/1000 MS (Java/Others)    Memory Limit: 65536/32768 K (Java/Others)
 Total Submission(s): 30284    Accepted Submission(s): 17531
