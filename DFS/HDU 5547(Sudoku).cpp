3b34df8c387e1d3a455deb571687699d503b8c08
ZYSzys <1220736035@qq.com>
2017-04-30 22:13:24 +0800

    Update HDU 5547(Sudoku).cpp

--- DFS/HDU 5547(Sudoku).cpp
+++ DFS/HDU 5547(Sudoku).cpp
@@ -145,3 +145,158 @@ int main()
     }
     return 0;
 }
+
+
+
+
+
+
+
+//自己TLE的垃圾代码:
+#include <set>
+#include <list>
+#include <deque>
+#include <queue>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#define INF 0x3f3f3f3f
+#define N 100010
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+char map[4][4];
+bool vis[4][4], flag;
+int cnt, jud, cas = 0;
+int a1, a2, a3, a4, c1, c2, c3, c4;
+void DFS(int x, int y, int time)
+{
+    if(flag)
+    {
+        return;
+    }
+    if(time > cnt)
+    {
+        return;
+    }
+    if(x<0||x>=4||y<0||y>=4)
+    {
+        return;
+    }
+    if(vis[x][y])
+    {
+        return;
+    }
+    if(time == cnt)
+    {
+        jud = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            c1 = 0, c2 = 0, c3 = 0, c4 = 0, a1 = 0, a2 = 0, a3 = 0, a4 = 0;
+            for(int j = 0; j < 4; j++)
+            {
+                if(map[i][j] == '1') c1++;
+                else if(map[i][j] == '2') c2++;
+                else if(map[i][j] == '3') c3++;
+                else c4++;
+                if(map[j][i] == '1') a1++;
+                else if(map[j][i] == '2') a2++;
+                else if(map[j][i] == '3') a3++;
+                else a4++;
+            }
+            if(c1 == 1 && c2 == 1 && c3 == 1 && c4 == 1)
+            {
+                jud++;
+            }
+            if(a1 == 1 && a2 == 1 && a3 == 1 && a4 == 1)
+            {
+                jud++;
+            }
+        }
+        if(jud != 8)
+        {
+            return;
+        }
+        if(map[0][0]+map[0][1]+map[1][0]+map[1][1] == '1'*4+6)
+        {
+            jud++;
+        }
+        if(map[0][2]+map[0][3]+map[1][2]+map[1][3] == '1'*4+6)
+        {
+            jud++;
+        }
+        if(map[2][0]+map[2][1]+map[3][0]+map[3][1] == '1'*4+6)
+        {
+            jud++;
+        }
+        if(map[2][2]+map[2][3]+map[3][2]+map[3][3] == '1'*4+6)
+        {
+            jud++;
+        }
+        if(jud == 12)
+        {
+            flag = true;
+            printf("Case #%d:\n", ++cas);
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    printf("%c", map[i][j]);
+                }
+                printf("\n");
+            }
+            return;
+        }
+    }
+    if(map[x][y] == '*')
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            map[x][y] = '1'+i;
+            vis[x][y] = true;
+            for(int j = 0; j < 4; j++)
+            {
+                DFS(x+dx[j], y+dy[j], time+1);
+            }
+            map[x][y] = '*';
+            vis[x][y] = false;
+        }
+    }
+    else
+    {
+        vis[x][y] = true;
+        for(int i = 0; i < 4; i++)
+        {
+            DFS(x+dx[i], y+dy[i], time);
+        }
+        vis[x][y] = false;
+    }
+}
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        cnt = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            scanf("%s", map[i]);
+            for(int j = 0; j < 4; j++)
+            {
+                if(map[i][j] == '*')
+                {
+                    cnt++;
+                }
+            }
+        }
+        memset(vis, false, sizeof(vis));
+        flag = false;
+        DFS(1, 1, 0);
+    }
+    return 0;
+}
