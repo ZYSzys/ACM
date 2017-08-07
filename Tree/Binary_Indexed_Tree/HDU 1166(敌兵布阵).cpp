b15d615f86cedcd7a52db420fd56645cf55474d5
章永胜 <1220736035@qq.com>
2017-08-07 14:25:57 +0800

    add Segment Tree solution to 1166

--- Tree/Binary_Indexed_Tree/HDU 1166(敌兵布阵).cpp
+++ Tree/Binary_Indexed_Tree/HDU 1166(敌兵布阵).cpp
@@ -49,11 +49,12 @@ Case 1:
 */
 
 
+//-------------------------------------------------树状数组
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
 using namespace std;
-const int N = 50050;
+const int N = 50010;
 int c[N], a[N];
 int n, t;
 int Lowbit(int x)
@@ -116,3 +117,91 @@ int main()
 	}
 	return 0;
 }
+
+
+
+//------------------------------------------------------线段树
+/*
+#include <cstdio>
+const int N = 50010;
+int sum[N<<2];
+void Build(int l, int r, int rt)
+{
+    if(l == r)
+    {
+        scanf("%d", &sum[rt]);
+        return;
+    }
+    int m = (l + r) >> 1;
+    Build(l, m, rt<<1);
+    Build(m+1, r, rt<<1|1);
+    sum[rt] = sum[rt<<1] + sum[rt<<1|1];
+}
+void Update(int p, int add, int l, int r, int rt)
+{
+    if(l == r)
+    {
+        sum[rt] += add;
+        return;
+    }
+    int m = (l + r) >> 1;
+    if(p <= m)
+    {
+        Update(p, add, l, m, rt<<1);
+    }
+    else
+    {
+        Update(p, add, m+1, r, rt<<1|1);
+    }
+    sum[rt] = sum[rt<<1] + sum[rt<<1|1];
+}
+int Query(int L, int R, int l, int r, int rt)
+{
+    if(l >= L && r <= R)
+    {
+        return sum[rt];
+    }
+    int m = (l + r) >> 1;
+    int ans = 0;
+    if(L <= m)
+    {
+        ans += Query(L, R, l, m, rt<<1);
+    }
+    if(R > m)
+    {
+        ans += Query(L, R, m+1, r, rt<<1|1);
+    }
+    return ans;
+}
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for(int i= 1; i <= t; i++)
+    {
+        printf("Case %d:\n", i);
+        int n, a, b;
+        scanf("%d", &n);
+        Build(1, n, 1);
+        char s[10];
+        while(~scanf("%s", s) && s[0] != 'E')
+        {
+            scanf("%d%d", &a, &b);
+            if(s[0] == 'Q')
+            {
+                printf("%d\n", Query(a, b, 1, n, 1));
+            }
+            else if(s[0] == 'S')
+            {
+                Update(a, -b, 1, n, 1);
+            }
+            else
+            {
+                Update(a, b, 1, n, 1);
+            }
+        }
+    }
+    return 0;
+}
+*/
+
